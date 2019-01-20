eeb421d7f6e6f69e7272af26bf38aa5d366bfc5c
pcdieren <46676469+pcdieren@users.noreply.github.com>
2019-01-20 19:26:15 +0100

    Add files via upload

---
+++ Polish - PL/words.php
@@ -0,0 +1,306 @@
+<?php
+/*
+------------------
+Language:   English
+Type:       Text
+------------------
+*/
+
+$langwords = array(
+"TXT_H_ACCEPT"               => "Accept",
+"TXT_H_ACCEPTATION"          => "Acceptation",
+"TXT_H_ACCEPTED"             => "Accepted",
+"TXT_H_ACCESSORIES"          => "Accessories",
+"TXT_H_ACCOUNT"              => "Account",
+"TXT_H_ACCOUNTANT"           => "Accountant",
+"TXT_H_ACCOUNTANTS"          => "Accountants",
+"TXT_H_ACHIEVED"             => "Achieved",
+"TXT_H_ACTION"               => "Action",
+"TXT_H_ACTIONS"              => "Actions",
+"TXT_H_ACTIVE"               => "Active",
+"TXT_H_AGE"                  => "Age",
+"TXT_H_AMOUNT"               => "Amount", //As in: number (not a price)
+"TXT_H_ANNOUNCEMENTS"        => "Announcements",
+"TXT_H_APPLICATIONS"         => "Applications", //Job Applications
+"TXT_H_APPLY"                => "Apply", //As in: apply a filter
+"TXT_H_APPROVE"              => "Approve",
+"TXT_H_APPROVED"             => "Approved",
+"TXT_H_ARCHIVE"              => "Archive",
+"TXT_H_ARCHIVED"             => "Archived",
+"TXT_H_ARRIVED"              => "Arrived",
+"TXT_H_ASSEMBLE"             => "Assemble",
+"TXT_H_AVAILABLE"            => "Available",
+"TXT_H_AVERAGE"              => "Average",
+"TXT_H_AWAKE"                => "Awake",
+
+"TXT_H_BACK"                 => "Back",
+"TXT_H_BUILDINGS"            => "Buildings",
+"TXT_H_BUY"                  => "Buy",
+
+"TXT_H_CANCEL"               => "Cancel",
+"TXT_H_CAPACITY"             => "Capacity",
+"TXT_H_CHANGE"               => "Change", //As in: changing something
+"TXT_H_CHAT"                 => "Chat",
+"TXT_H_CHEMICAL"             => "Chemical",
+"TXT_H_CITY"                 => "City",
+"TXT_H_CLOSED"               => "Closed",
+"TXT_H_COMPANIES"            => "Companies",
+"TXT_H_COMPANY"              => "Company",
+"TXT_H_COMPLETE"             => "Complete", //As in % complete
+"TXT_H_COMPLETED"            => "Completed",
+"TXT_H_COMPLETION"           => "Completion",
+"TXT_H_CONCERN"              => "Corporation",
+"TXT_H_CONCERNS"             => "Corporations",
+"TXT_H_CONDITION"            => "Condition",
+"TXT_H_CONTACT"              => "Contact",
+"TXT_H_CONTAINER"            => "Container",
+"TXT_H_CONTRACT"             => "Contract",
+"TXT_H_CONTRACTS"            => "Contracts",
+"TXT_H_COOLED"               => "Cooled",
+"TXT_H_COST"                 => "Cost", //As in: the total cost of something
+"TXT_H_COUNTERPARTY"         => "Counterparty",
+"TXT_H_COUNTRIES"            => "Countries",
+"TXT_H_COUNTRY"              => "Country",
+"TXT_H_CREATE"               => "Create",
+"TXT_H_CREDIT"               => "Credit",
+"TXT_H_CREDITS"              => "Credits",
+"TXT_H_CURRENT"              => "Current", //As in: current level
+"TXT_H_CURRENTLY"            => "Currently",
+
+"TXT_H_DASHBOARD"            => "Dashboard",
+"TXT_H_DATE"                 => "Date",
+"TXT_H_DAY"                  => "Day",
+"TXT_H_DAYS"                 => "Days",
+"TXT_H_DEFAULT"              => "Default",
+"TXT_H_DELETE"               => "Delete",
+"TXT_H_DELIVERY"             => "Delivery",
+"TXT_H_DEPARTURE"            => "Departure",
+"TXT_H_DEPRECIATION"         => "Depreciation",
+"TXT_H_DESCRIPTION"          => "Description",
+"TXT_H_DESTINATION"          => "Destination",
+"TXT_H_DETAILS"              => "Details",
+"TXT_H_DISASSEMBLE"          => "Disassemble",
+"TXT_H_DISCOUNT"             => "Discount",
+"TXT_H_DISTANCE"             => "Distance",
+"TXT_H_DONATE"               => "Donate",
+"TXT_H_DONATION"             => "Donation",
+"TXT_H_DONATIONS"            => "Donations",
+"TXT_H_DONE"                 => "Done",
+"TXT_H_DOWNGRADE"            => "Downgrade",
+"TXT_H_DRIVE"                => "Drive",
+"TXT_H_DRIVING"              => "Driving",
+
+"TXT_H_EARNINGS"             => "Earnings",
+"TXT_H_ECONOMY"              => "Economy", //As in: how much fuel a truck uses
+"TXT_H_EMAIL"                => "E-mail",
+"TXT_H_EMPLOYEE"             => "Employee",
+"TXT_H_EMPLOYEES"            => "Employees",
+"TXT_H_ENEMIES"              => "Enemies",
+"TXT_H_EXAM"                 => "Exam", //As in: button to take an exam
+"TXT_H_EXPENSES"             => "Expenses",
+"TXT_H_EXPIRES"              => "Expires",
+"TXT_H_EXTRA"                => "Extra",
+
+"TXT_H_FAILED"               => "Failed",
+"TXT_H_FAQ"                  => "FAQ", //Frequently Asked Questions
+"TXT_H_FILTER"               => "Filter",
+"TXT_H_FINANCE"              => "Finance",
+"TXT_H_FINISH"               => "Finish",
+"TXT_H_FINISHING"            => "Finishing",
+"TXT_H_FIRE"                 => "Fire",
+"TXT_H_FREE"                 => "Free", //As in: no costs
+"TXT_H_FREIGHT"              => "Freight",
+"TXT_H_FREIGHTS"             => "Freights",
+"TXT_H_FRIENDS"              => "Friends",
+"TXT_H_FUNCTION"             => "Function",
+"TXT_H_FUEL"                 => "Fuel",
+"TXT_H_FULL"                 => "Full",
+
+"TXT_H_GARAGE"               => "Garage",
+"TXT_H_GENERAL"              => "General",
+"TXT_H_GROUPED"              => "Grouped",
+
+"TXT_H_HEADQUARTER"          => "Headquarter",
+"TXT_H_HEAVY"                => "Heavy",
+"TXT_H_HIRE"                 => "Hire",
+"TXT_H_HIRED"                => "Hired",
+"TXT_H_HP"                   => "HP",
+"TXT_H_HQ"                   => "HQ",
+"TXT_H_HOME"                 => "Home",
+"TXT_H_HOUR"                 => "Hour", //As in: 1 hour
+"TXT_H_HOURS"                => "Hours", //As in: 2 hours
+
+"TXT_H_INFORMATION"          => "Information",
+"TXT_H_INSURANCE"            => "Insurance",
+"TXT_H_INVOICES"             => "Invoices",
+
+"TXT_H_JOINED"               => "Joined",
+
+"TXT_H_LANGUAGE"             => "Language",
+"TXT_H_LEAVE"                => "Leave",
+"TXT_H_LEVEL"                => "Level",
+"TXT_H_LICENSE"              => "License",
+"TXT_H_LIQUID"               => "Liquid",
+"TXT_H_LITER"                => "Liter", //Amount before this - singular
+"TXT_H_LITERS"               => "Liters", //Amount before this - plural
+"TXT_H_LOAD"                 => "Load",
+"TXT_H_LOADED"               => "Loaded", // Yes or No after this
+"TXT_H_LOADING"              => "Loading",
+"TXT_H_LOCATION"             => "Location",
+"TXT_H_LOCATIONS"            => "Locations",
+"TXT_H_LOGS"                 => "Logs",
+"TXT_H_LOW"                  => "Low",
+
+"TXT_H_MAINTENANCE"          => "Maintenance",
+"TXT_H_MANAGEMENT"           => "Management",
+"TXT_H_MANAGER"              => "Manager",
+"TXT_H_MANAGERS"             => "Managers",
+"TXT_H_MEMBER"               => "Member",
+"TXT_H_MEMBERS"              => "Members",
+"TXT_H_MESSAGE"              => "Message",
+"TXT_H_MESSAGES"             => "Messages",
+"TXT_H_MILEAGE"              => "Mileage",
+"TXT_H_MONEY"                => "Money",
+"TXT_H_MOUNTED"              => "Mounted",
+
+"TXT_H_NAME"                 => "Name",
+"TXT_H_NEW"                  => "New",
+"TXT_H_NEXT"                 => "Next",
+"TXT_H_NO"                   => "No",
+"TXT_H_NONE"                 => "None",
+"TXT_H_NOTHING"              => "Nothing",
+"TXT_H_NOTIFICATIONS"        => "Notifications",
+"TXT_H_NOW"                  => "Now",
+
+"TXT_H_OBJECT"               => "Object",
+"TXT_H_OBJECTS"              => "Objects",
+"TXT_H_OFFICES"              => "Offices",
+"TXT_H_OFFLINE"              => "Offline",
+"TXT_H_ONLINE"               => "Online",
+"TXT_H_OTHER"                => "Other",
+"TXT_H_OVERVIEW"             => "Overview",
+"TXT_H_OWNER"                => "Owner",
+
+"TXT_H_PAGE"                 => "Page",
+"TXT_H_PAID"                 => "Paid",
+"TXT_H_PARKED"               => "Parked",
+"TXT_H_PARTNERS"             => "Partners",
+"TXT_H_PAY"                  => "Pay",
+"TXT_H_PENALTY"              => "Penalty",
+"TXT_H_PENDING"              => "Pending", //As in: Open job applications
+"TXT_H_PLAYER"               => "Player",
+"TXT_H_PLUS"                 => "Plus", //Insurance type
+"TXT_H_POINTS"               => "Points",
+"TXT_H_POWER"                => "Power", //As in: Truck (engine) power
+"TXT_H_PREMIUM"              => "Premium",
+"TXT_H_PREVIOUS"             => "Previous",
+"TXT_H_PRICE"                => "Price",
+"TXT_H_PRIVATE"              => "Private",
+"TXT_H_PROFILE"              => "Profile",
+"TXT_H_PROJECT"              => "Project",
+"TXT_H_PROJECTS"             => "Projects",
+"TXT_H_PROGRESS"             => "Progress",
+"TXT_H_PROMOTE"              => "Promote",
+"TXT_H_PURCHASED"            => "Purchased",
+
+"TXT_H_QUANTITY"             => "Quantity",
+"TXT_H_QUESTIONS"            => "Questions",
+
+"TXT_H_RADIOACTIVE"          => "Radioactive",
+"TXT_H_RANDOM"               => "Random",
+"TXT_H_READY"                => "Ready",
+"TXT_H_RECEIVER"             => "Receiver",
+"TXT_H_REFILL"               => "Refill",
+"TXT_H_REFUEL"               => "Refuel",
+"TXT_H_REFUELING"            => "Refueling",
+"TXT_H_REFRESH"              => "Refresh",
+"TXT_H_REGISTERED"           => "Registered",
+"TXT_H_REJECT"               => "Reject",
+"TXT_H_REJECTED"             => "Rejected",
+"TXT_H_RENEW"                => "Renew",
+"TXT_H_REPAIR"               => "Repair",
+"TXT_H_REPLY"                => "Reply", //As in: Reply to message
+"TXT_H_REQUIRED"             => "Required",
+"TXT_H_REQUIREMENT"          => "Requirement",
+"TXT_H_RIGHTS"               => "Rights",
+"TXT_H_RULES"                => "Rules",
+
+"TXT_H_SALARY"               => "Salary",
+"TXT_H_SAVE"                 => "Save", //As in: save settings
+"TXT_H_SEARCH"               => "Search",
+"TXT_H_SELECT"               => "Select",
+"TXT_H_SELECTED"             => "Selected",
+"TXT_H_SELL"                 => "Sell",
+"TXT_H_SEND"                 => "Send",
+"TXT_H_SENDER"               => "Sender",
+"TXT_H_SENT"                 => "Sent",
+"TXT_H_SET"                  => "Set", //As in: Set new settings
+"TXT_H_SETTINGS"             => "Settings",
+"TXT_H_SHOP"                 => "Shop", //Object - The shop
+"TXT_H_SICK"                 => "Sick",
+"TXT_H_SLEEP"                => "Sleep", //To sleep
+"TXT_H_SLEEPING"             => "Sleeping",
+"TXT_H_STANDARD"             => "Standard",
+"TXT_H_STATISTICS"           => "Statistics",
+"TXT_H_STATUS"               => "Status",
+"TXT_H_SUBJECT"              => "Subject",
+"TXT_H_SYSTEM"               => "System",
+
+"TXT_H_TASK"                 => "Task",
+"TXT_H_TASKS"                => "Tasks",
+"TXT_H_TAXES"                => "Taxes",
+"TXT_H_TECHNICAL"            => "Technical",
+"TXT_H_TECHNICIANS"          => "Technicians", //As in: the people who repair your trucks
+"TXT_H_TERM"                 => "Term",
+"TXT_H_TIME"                 => "Time",
+"TXT_H_TIMEZONE"             => "Timezone",
+"TXT_H_TIPPER"               => "Tipper", //As in: a type trailer
+"TXT_H_TIPS"                 => "Tips",
+"TXT_H_TODAY"                => "Today",
+"TXT_H_TOPSPEED"             => "Topspeed",
+"TXT_H_TOTAL"                => "Total",
+"TXT_H_TRAILER"              => "Trailer",
+"TXT_H_TRAILERS"             => "Trailers",
+"TXT_H_TRANSFER"             => "Transfer", //To transfer, as in: transfer a truck to another location
+"TXT_H_TRIP"                 => "Trip",
+"TXT_H_TRIPS"                => "Trips",
+"TXT_H_TRUCK"                => "Truck",
+"TXT_H_TRUCKER"              => "Trucker",
+"TXT_H_TRUCKERS"             => "Truckers",
+"TXT_H_TRUCKS"               => "Trucks",
+"TXT_H_TUTORIAL"             => "Tutorial",
+"TXT_H_TYPE"                 => "Type",
+
+"TXT_H_UNAVAILABLE"          => "Unavailable",
+"TXT_H_UNLOAD"               => "Unload",
+"TXT_H_UNLOADED"             => "Unloaded",
+"TXT_H_UNLOADING"            => "Unloading",
+"TXT_H_UNLIMITED"            => "Unlimited",
+"TXT_H_UNLOCK"               => "Unlock", //As in: unlock new features
+"TXT_H_UNLOCKED"             => "Unlocked",
+"TXT_H_UPGRADE"              => "Upgrade", //To upgrade
+"TXT_H_USER"                 => "User",
+
+"TXT_H_VALUE"                => "Value",
+"TXT_H_VIEW"                 => "View", //To view
+
+"TXT_H_WAITING"              => "Waiting", //As in: the truck is waiting for something
+"TXT_H_WAREHOUSE"            => "Warehouse",
+"TXT_H_WEEK"                 => "Week",
+
+"TXT_H_YES"                  => "Yes",
+
+//Words - L
+"TXT_KMH"                    => "km/h",
+"TXT_KM"                     => "km",
+"TXT_DAYS"                   => "days",
+"TXT_HRS"                    => "hrs",
+"TXT_HOUR"                   => "hour",
+"TXT_DAY"                    => "day", // *var* per day
+"TXT_LITER"                  => "liter",  // km/liter - singular
+"TXT_MINUTE"                 => "minute",  // *var* per minute - singular
+"TXT_INSURED"                => "insured", // *var* % insured
+"TXT_OR"                     => "or",
+"TXT_NOT_APPLICABLE_SHORT"   => "n/a" //Abbreviation of 'Not Applicable'
+);
+?>
---
+++ Turkish - TR/specials.php
@@ -0,0 +1,87 @@
+<?php
+
+$langspecials = array(
+"TXT_FAQ_Q_1"        => "Why is a newer truck better than an old one?",
+"TXT_FAQ_A_1"        => "If you have a truck with a lower age, it will require less maintenance.",
+
+"TXT_FAQ_Q_2"        => "Why are some functions for premium users only?",
+"TXT_FAQ_A_2"        => "The refuel all function and 14 days finance overview need a lot of performance from our server. That's the reason we have chosen to limit it to premium users only.\n\n We have also made our Create Corporation option a premium one because we don't want just as much corporations as there are companies in this game.",
+
+"TXT_FAQ_Q_3"        => "Which freight distance gives me the most money?",
+"TXT_FAQ_A_3"        => "In fact, it's always the best to take a shorter trip, because you will get more money in less time, but if you do so, you'll have to keep an eye on your freights the whole time. So only if you stay away for a longer period of time, it's better to accept a longer trip.\n\n But there's more; if you have an older truck or trailer (as in age of the truck/trailer), you will even get less money for a longer trip, because the maintenance cost will be really high.\n\n So if you want to stay away for longer periods of time and because of that, accept longer trips, you should buy a newer truck/trailer. Just to be clear; not one that's more expensive, but one that's less old.",
+
+"TXT_FAQ_Q_5"        => "For whatever reason I'm stuck at a city. What should I do?",
+"TXT_FAQ_A_5"        => "You can always transfer your trucks, trailers and employees to another city if needed. Just go to your garage to select a truck or trailer or go to your employee overview and press the transfer button.",
+
+"TXT_FAQ_Q_6"        => "When does my company get an invoice?",
+"TXT_FAQ_A_6"        => "Your company will get an invoice if the needed daily payments can't be automatically debited from your payment account. So this will happen if you don't have enough money or your money is on your savings account.",
+
+"TXT_FAQ_Q_7"        => "Is it allowed to have multiple accounts at the same house?",
+"TXT_FAQ_A_7"        => "Yes, that is allowed in this game. As long as you don't abuse it.",
+
+"TXT_FAQ_Q_8"        => "What do you need a headquarter for?",
+"TXT_FAQ_A_8"        => "A headquarter is the location where your new trucks and trailers will be delivered.",
+
+"TXT_FAQ_Q_9"        => "What happens if I abuse my position as a corporation owner?",
+"TXT_FAQ_A_9"        => "It's possible we will take back your posibility to start a corporation. If you paid the premium option for starting a corporation, we won't give your premium credits back.",
+
+"TXT_FAQ_Q_10"       => "What is a corporation?", 
+"TXT_FAQ_A_10"       => "Companies are able to join a corporation. How much companies can join depends on the amount and level of offices the corporation has bought. \n \n A corporation can buy fuel stations and refuel them in an hour of which the fuel price is at it's lowest (fuel prices change every hour). As the corporation owner you can then set a specific fuel price, so the companies in your corporation are able to refuel at a time in which the public fuel price is higher.\n\n Last but not least; every hour your corporation will get corporation freights. These freights will give the company that accepts the freight more money than a normal trip.",
+
+"TXT_FAQ_Q_11"       => "When do you need a Fuel Tank?",
+"TXT_FAQ_A_11"       => "A Fuel Tank is currently only for storing fuel that you've stolen from another companies.",
+
+"TXT_FAQ_Q_12"       => "Can I use the Speed Up buttons on the iOS app as well?",
+"TXT_FAQ_A_12"       => "No, unfortunately due to Apple's policies, it is not possible to use the Speed Up buttons in the iOS app.",
+
+"TXT_FAQ_Q_13"       => "If I buy a premium account or fast action times, can I use it in the iOS and Android app?",
+"TXT_FAQ_A_13"       => "Yes, you will be able to use those in the iOS and Android app as well. \n\n In the iOS app the functions and buttons related to the premium functions are hidden, but they will appear when you've bought the functions on the website.",
+
+"TXT_FAQ_Q_14"       => "What happens if I buy premium on the Android app?",
+"TXT_FAQ_A_14"       => "If you buy premium functions on the Android app, you will be able to use them on the website and iOS app as well. There is no difference between buying premium functions on the website or the Android app.",
+
+"TXT_FAQ_Q_15"       => "How does my level progress?",
+"TXT_FAQ_A_15"       => "Your level will progress everytime you finish a freight.",
+
+"TXT_FAQ_Q_16"       => "My employee called in sick. What should I do?",
+"TXT_FAQ_A_16"       => "You'll have to choose another employee for your freights, until he gets better. This will take at least 12 hours.",
+
+"TXT_FAQ_Q_17"       => "Why is the maintenance of my truck/trailer so high?",
+"TXT_FAQ_A_17"       => "If your truck or trailer requires a lot of maintenance, it is probably too old. The older he gets (number of days), the more maintenance he needs.",
+
+"TXT_FAQ_Q_18"       => "Can I restart the tutorial?",
+"TXT_FAQ_A_18"       => "You can restart the tutorial only by deleting your company. The tutorial will then automatically be restarted.",
+
+"TXT_TUTORIAL_1"             => "The first step in this game is to create a company.",
+"TXT_TUTORIAL_2"             => "You have got some default trucks and trailers. Go check them out!",
+"TXT_TUTORIAL_3"             => "Press the information button of a truck or trailer.",
+"TXT_TUTORIAL_4"             => "The fuel tank of your trucks is empty. Go to the fuel station to refuel one of them.",
+"TXT_TUTORIAL_5"             => "Please refill your trucks.",
+"TXT_TUTORIAL_6"             => "Great! Your truck is filling up. Let's check out the trips you can accept.",
+"TXT_TUTORIAL_7"             => "Better freights will give you more money, but require a better trailer and transport license. Go check out your transport license.",
+"TXT_TUTORIAL_8"             => "When you level up, you can get a better license here. For now, let's take a look at the shop to get some buildings.",
+"TXT_TUTORIAL_9"             => "We will need a warehouse and a garage. Please buy one.",
+"TXT_TUTORIAL_10"            => "Great! Now we have to hire some employees.",
+"TXT_TUTORIAL_11"            => "You will need a trucker and a warehouse employee on your headquarter location. Hire them and go to the Current Team panel to check them out.",
+"TXT_TUTORIAL_12"            => "Please click on one of your employees.",
+"TXT_TUTORIAL_13"            => "Here you can see all the actions you can take care of for your employees. Now let's get back to the available trips to start earning some money!",
+"TXT_TUTORIAL_14"            => "Shorter trips will give you more money (if you have deducted the costs). Just accept the shortest trip for now.",
+"TXT_TUTORIAL_15"            => "Great! Please go to your warehouse to open the freight.",
+"TXT_TUTORIAL_16"            => "Click on the freight to open it.",
+"TXT_TUTORIAL_17"            => "We have to load this freight and we need a trailer and warehouse employee for that. Select them first.",
+"TXT_TUTORIAL_18"            => "Great! Now start loading your freight.",
+"TXT_TUTORIAL_19"            => "You can wait or do the same actions with another freight. After that, select a truck and trucker and start driving.",
+"TXT_TUTORIAL_20"            => "Great! Your truck is on his way to his destination. When he has arrived, you can unload your freight.",
+"TXT_TUTORIAL_21"            => "When your trailer has been unloaded, your freight is ready to be closed.",
+"TXT_TUTORIAL_22"            => "You can now finish your freight. Go get your money!",
+"TXT_TUTORIAL_23"            => "When you have earned some money, you can buy new trucks and trailers. Go to the truck store to check them out.",
+"TXT_TUTORIAL_24"            => "These are the trucks, now let's take a look at the trailers.",
+"TXT_TUTORIAL_25"            => "You want to see how other companies are working their way up? Go to the company list and check out another company.",
+"TXT_TUTORIAL_26"            => "I guess you can find out the rest of the game by yourself right? Just go back to the homepage to complete this tutorial.",
+
+"TXT_TIPS_1"                 => "If you have questions about the game, try to find an answer in the FAQ section, which can be found at the General menu.",
+"TXT_TIPS_2"                 => "Did you find bugs or do you have some improvements for the game? Let us know at info@logitycoon.com",
+"TXT_TIPS_3"                 => "When your freight is not completed in time, you will lose points every hour, until you have lost 4 points.",
+"TXT_TIPS_4"                 => "If you have older trucks or trailers, it is possible that due to the maintenance costs, a longer freight will give you less money than a short one.",
+);
+?>
