084131910fa1cc830608df2841161740b9d04eee
pcdieren <46676469+pcdieren@users.noreply.github.com>
2019-01-20 10:55:04 +0100

    Add files via upload

---
+++ English (Base)/multi_words.php
@@ -0,0 +1,408 @@
+<?php
+
+
+$langmultiwords = array(
+"TXT_H_COMP_PROPERTIES"      => "Company Properties", //As in: things that you own
+"TXT_H_LAST_TRANSACTION"     => "Last transaction",
+"TXT_H_NEWS"                 => "Latest News",
+
+"TXT_MEMBER_ACTIVITY"        => "Member Activity",
+"TXT_H_NO_TRANSACTIONS"      => "No transactions",
+"TXT_PRIVACYPOLICY"          => "Privacy Policy",
+"TXT_H_STARTTIME"            => "Start time",
+"TXT_TERMSOFUSE"             => "Terms of Use",
+"TXT_TOP_WEEK"               => "Top This Week",
+"TXT_TOP_TODAY"              => "Top Today",
+"TXT_TOP_TOTAL"              => "Top Total",
+
+"TXT_USERNAME_NOT_AVAILABLE" => "Username Not Available.",
+"TXT_USERNAME_AVAILABLE"     => "Username Available.",
+
+"TXT_VIEWALL"                => "View all",
+
+"TXT_H_YOURCOMPANY"          => "Your Company",
+
+"TXT_NOT_SET"                => "Not set",
+"TXT_FREIGHT_NOTIFS"         => "freight notifications", //Number before this
+"TXT_ACTIVE_FREIGHTS"        => "active freights", //Number before this
+"TXT_COMP_NOTIFS"            => "company notifications", //Number before this
+"TXT_NEW_MSGS"               => "New messages",
+"TXT_NO_NEW_MSGS"            => "No new messages",
+"TXT_PENDING_INVOICES"       => "pending invoices", //Number before this
+"TXT_MY_PROFILE"             => "My Profile",
+"TXT_MY_CALENDAR"            => "My Calendar",
+"TXT_MY_INBOX"               => "My Inbox",
+"TXT_CHANGE_PSW"             => "Change Password",
+"TXT_CHANGE_SETTING"         => "Change Setting",
+"TXT_LOG_OUT"                => "Log Out",
+
+"TXT_COMP_PROFILE"           => "Company Profile",
+"TXT_CONCERN_PROFILE"        => "Corporation Profile",
+"TXT_USER_PROFILE"           => "User Profile",
+
+"TXT_DAILY_AWARD"            => "Daily Award",
+"TXT_GENERAL_MESSAGE"        => "General Message",
+
+"TXT_FUEL_TANK"              => "Fuel Tank",
+"TXT_TRUCK_TRAILER_MARKET"   => "Truck & Trailer Market",
+"TXT_JOIN_CONCERN"           => "Join Corporation",
+"TXT_BANK_ACCOUNT"           => "Bank Account",
+"TXT_NAVIGATION_UNITS"       => "Navigation Units",
+"TXT_FUEL_TANK_LOCK"         => "Fuel Tank Lock",
+"TXT_CHANGE_INSURANCE"       => "Change Insurance",
+"TXT_HR_MANAGERS"            => "HR Managers",
+"TXT_TECHNICAL_MANAGERS"     => "Technical Managers",
+
+
+"TXT_EMPLOYEES_UPDATE"       => "Employees Update", //A daily update on information about your employees
+"TXT_TRUCK_DAMAGED"          => "Truck Damaged",
+"TXT_TRAILER_DAMAGED"        => "Trailer Damaged",
+
+"TXT_ALL_COMPANIES"          => "All Companies",
+"TXT_RECORDS_FOUND"          => "Records Found",
+
+"TXT_H_COMPANY_POINTS"       => "Company Points",
+"TXT_BASED_ON"               => "Based on", // : after this
+
+"TXT_LAST_24_HRS"            => "Last 24 Hours",
+"TXT_LAST_7_DAYS"            => "Last 7 days",
+"TXT_LAST_14_DAYS"           => "Last 14 days",
+"TXT_LAST_30_DAYS"           => "Last 30 days",
+
+"TXT_FREIGHTS_COMPLETED"     => "Freights completed",
+"TXT_OPEN_FREIGHTS"          => "Open Freights",
+"TXT_OPEN_FREIGHT"           => "Open Freight",
+
+"TXT_KMS_DRIVEN"             => "KM's driven",
+"TXT_LOW_FUEL"               => "Low fuel",
+"TXT_BAD_CONDITION"          => "Bad condition",
+"TXT_LOW_AWAKE_PERC"         => "Low awake %",
+
+"TXT_NEW_STATUS"             => "New status", //: after this or in a table
+
+"TXT_HIGH_SCORE_RANK"        => "High Score Rank",
+"TXT_PRIVATE_COMPANY"        => "Private Company",
+"TXT_PART_OF_CONCERN"        => "Part of Corporation", //: after this
+"TXT_PART_OF_A_CONCERN"      => "Part of a Corporation",
+
+"TXT_DELETE_COMPANY"         => "Delete Company",
+"TXT_DELETE_CONCERN"         => "Delete Corporation",
+
+"TXT_DAYS_OLD"               => "days old", //Amount before this
+
+"TXT_STEAL_FUEL"             => "Steal Fuel",
+"TXT_FUEL_TANK_PROTECTED"    => "Fuel Tank protected!",
+
+"TXT_AVAILABLE_AT"           => "Available at", //Countdown Time after this
+"TXT_HIGHER_LEVEL_REQ"       => "Higher Level required!",
+
+"TXT_FIRE_MEMBER"            => "Fire Member",
+"TXT_PROMOTE_MEMBER"         => "Promote Member",
+"TXT_APPOINT_AS_CEO"         => "Appoint as CEO",
+
+"TXT_TOTAL_MEMBERS"          => "Total members", // : after this
+"TXT_THIS_WEEK"              => "This week",
+"TXT_USER_LEVEL"             => "User Level",
+"TXT_DONATIONS_OVERVIEW"     => "Donations overview",
+"TXT_AUTO_DAILY_DONATE"      => "Daily donation",
+"TXT_CURRENT_AMOUNT"         => "Current amount", //As in: amount of money
+"TXT_NEW_AMOUNT"             => "New amount", //As in: amount of money
+"TXT_DONATE_MONEY"           => "Donate money",
+
+"TXT_FUEL_LOGS"              => "Fuel Logs",
+"TXT_LITER_PRICE"            => "Liter Price",
+
+"TXT_ACCEPTS_COMPANIES"      => "Accepts companies", // : yes or no after this
+"TXT_CONCERN_POINTS"         => "Corporation points",
+"TXT_YOUR_CONCERN"           => "Your Corporation",
+"TXT_LEAVE_CONCERN"          => "Leave Corporation",
+"TXT_MY_CONCERN"             => "My Corporation",
+"TXT_MANAGE_CONCERN"         => "Manage Corporation",
+"TXT_CONCERN_PROJECTS"       => "Corporation Projects",
+"TXT_CREATE_CONCERN"         => "Create Corporation",
+
+"TXT_FUEL_STATION"           => "Fuel Station",
+"TXT_FUEL_STATIONS"          => "Fuel Stations",
+"TXT_CONCERN_FREIGHTS"       => "Corporation Freights",
+"TXT_CONCERN_FREIGHT"        => "Corporation Freight",
+"TXT_CONCERN_PROJECT"        => "Corporation Project",
+
+"TXT_ONLINE_MEMBERS"         => "Online members",
+"TXT_CURRENT_MEMBERS"        => "Current Members",
+"TXT_MAX_MEMBERS"            => "Max. Members",
+"TXT_ALL_MEMBERS"            => "All Members",
+
+
+"TXT_UPGRADE_COST"           => "Upgrade cost",
+"TXT_CONCERN_LEVEL"          => "Corporation Level",
+"TXT_UPGRADE_OFFICES"        => "Upgrade Offices",
+"TXT_CURRENT_OFFICES"        => "Current Offices",
+"TXT_BUY_MORE_OFFICES"       => "Buy Offices",
+
+"TXT_CONCERN_OWNER"          => "Corporation Owner",
+"TXT_NO_INTEREST"            => "No interest", //As in: a daily amount of money you get
+
+"TXT_APPLY_CONCERN"          => "Apply to Corporation",
+"TXT_CANCEL_APPLICATION"     => "Cancel Application",
+
+"TXT_OTHER_CONCERNS"         => "Other Corporations",
+
+"TXT_CANCEL_CONC_PROJECT"    => "Cancel Corporation Project",
+
+"TXT_ACCEPT_CONTRACT"        => "Accept contract",
+"TXT_ACCEPTED_CONTRACTS"     => "Accepted Contracts",
+"TXT_TECHNICAL_MANAGER"      => "Technical Manager",
+"TXT_HR_MANAGER"             => "HR Manager",
+"TXT_ADD_TO_CONCERN"         => "Add to Corporation",
+"TXT_TRANSPORT_LICENSE"      => "Transport License",
+"TXT_OPEN_INVOICES"          => "Open Invoices",
+"TXT_NOT_DONE"               => "Not done",
+"TXT_DRIVING_LICENSE"        => "Driving License",
+"TXT_CANCEL_FREIGHT"         => "Cancel Freight",
+"TXT_WAREHOUSE_EMPLOYEE"     => "Warehouse Employee",
+"TXT_FREIGHT_OVERVIEW"       => "Freight Overview",
+"TXT_SELECT_ONE"             => "Select one",
+"TXT_UNIQUE_ID"              => "Unique ID",
+"TXT_CURRENT_FREIGHT"        => "Current Freight",
+"TXT_WAREHOUSE_EMPLOYEES"    => "Warehouse Employees",
+"TXT_PREMIUM_CREDITS"        => "Premium credits",
+"TXT_LAST_ONLINE"            => "Last online",
+
+"TXT_CONCERN_FOUNDER"        => "Corporation Founder",
+"TXT_REMAINING_TASKS"        => "Remaining Tasks",
+"TXT_NO_REMAINING_TASKS"     => "No Remaining Tasks",
+"TXT_SELECT_A_TASK"          => "Select a task",
+"TXT_FREIGHT_PROGRESS"       => "Freight Progress",
+"TXT_MARK_AS_SEEN"           => "Mark as Seen",
+"TXT_USED_TRAILERS"          => "Used trailers",
+
+"TXT_PLACEMENT_DATE"         => "Placement date",
+
+"TXT_REQUIRED_LEVEL"         => "Required level",
+"TXT_ORIGINAL_VALUE"         => "Original value",
+"TXT_REPAIR_PRICE"           => "Repair Price",
+"TXT_BUY_THIS_TRAILER"       => "Buy this trailer",
+"TXT_CANCEL_SALE"            => "Cancel Sale",
+"TXT_NOT_MOUNTED"            => "Not mounted",
+"TXT_NAVIGATION_UNIT"        => "Navigation Unit",
+"TXT_BUY_THIS_TRUCK"         => "Buy this truck",
+"TXT_CURRENT_FUEL"           => "Current Fuel",
+"TXT_TANK_CAPACITY"          => "Tank Capacity",
+"TXT_CURRENT_VALUE"          => "Current Value",
+"TXT_FUEL_VALUE"             => "Fuel Value",
+"TXT_TOTAL_VALUE"            => "Total Value",
+"TXT_CURRENT_DEALER_VALUE"   => "Current Dealer Value",
+"TXT_ASKING_PRICE"           => "Asking Price",
+
+"TXT_SKIP_TUTORIAL"          => "Skip Tutorial",
+"TXT_EXPENSIVE_TRUCKS"       => "Expensive trucks",
+"TXT_CHEAP_TRUCKS"           => "Cheap trucks",
+"TXT_USED_TRUCKS"            => "Used trucks",
+"TXT_YOU_HAVE"               => "You have", // : * amount after this
+"TXT_CHEAP_TRAILERS"         => "Cheap Trailers",
+"TXT_EXPENSIVE_TRAILERS"     => "Expensive Trailers",
+
+"TXT_CURRENT_LOCATION"       => "Current location",
+"TXT_NEW_LOCATION"           => "New location",
+"TXT_START_TRANSFER"         => "Start transfer",
+
+"TXT_ACTIVE_PREMIUM_FUNC"    => "Active Premium Functions",
+"TXT_USE_CREDITS"            => "Use Credits",
+"TXT_BUY_CREDITS"            => "Buy Credits",
+"TXT_ACTIVE_FUNCTIONS"       => "Active Functions",
+"TXT_UNLIMITED_DAYS"         => "Unlimited days",
+"TXT_PAYMENT_DATE"           => "Payment date",
+"TXT_PAY_ALL"                => "Pay All",
+"TXT_OUTSTANDING_INVOICES"   => "Outstanding invoices",
+
+"TXT_MAXIMUM_DISTANCE"       => "Maximum distance",
+"TXT_REQUIRED_POWER"         => "Required Power",
+"TXT_REQUIRED_TRAILER"       => "Required Trailer",
+
+"TXT_CURRENT_INSURANCE"      => "Current Insurance",
+"TXT_CURRENT_PRICE"          => "Current Price",
+"TXT_NO_INSURANCE"           => "No insurance",
+"TXT_FULL_INSURANCE"         => "Full insurance",
+"TXT_OWN_DAMAGE"             => "own damage", // + *var* % own damage
+"TXT_SPEED_UP"               => "Speed up",
+"TXT_MAINTENANCE_PRICE"      => "Maintenance Price",
+"TXT_MAINTENANCE_TIME"       => "Maintenance Time",
+"TXT_LAST_MAINTENANCE"       => "Last Maintenance",
+"TXT_SELL_TO_DEALER"         => "Sell to dealer",
+"TXT_CURRENT_ACTION"         => "Current Action",
+"TXT_NEW_PRICE"              => "New price", // As in 'Changed price'
+"TXT_FINANCIAL_OVERVIEW"     => "Financial Overview",
+
+"TXT_MY_COMPANY"             => "My Company",
+"TXT_CREATE_COMPANY"         => "Create Company",
+"TXT_MANAGE_COMPANY"         => "Manage Company",
+
+"TXT_AVAILABLE_TRIPS"        => "Available Trips",
+"TXT_ALL_TRIPS"              => "All Trips",
+
+"TXT_GENERAL_SHOP"           => "General Shop",
+"TXT_HIRE_EMPLOYEES"         => "Hire Employees",
+"TXT_TRUCK_DEALER"           => "Truck Dealer",
+"TXT_TRUCK_MARKET"           => "Truck Market",
+
+"TXT_DELETE_ALL_CHATS"       => "Delete all chats",
+"TXT_CHANGE_LANGUAGE"        => "Change Language",
+"TXT_TOTAL_COSTS"            => "Total Costs",
+
+"TXT_PAYMENT_ACCOUNT"        => "Payment Account",
+"TXT_SAVINGS_ACCOUNT"        => "Savings Account",
+"TXT_NO_COMPANY"             => "No Company",
+"TXT_NO_CONCERN"             => "No Corporation",
+"TXT_NO_FUEL_TANK"           => "No Fuel Tank",
+"TXT_TRANSFER_MONEY"         => "Transfer money",
+"TXT_TRANSFER_MONEY_TO"      => "Transfer money to", // : after this
+
+"TXT_COMPANY_REMOVED"        => "Company Removed",
+
+"TXT_NOT_AVAILABLE"          => "Not Available",
+"TXT_ALL_IN_USE"             => "All in use",
+
+"TXT_RECOMMENDED_TRIPS"      => "Recommended Trips",
+"TXT_SELECT_COUNTRY"         => "Select Country",
+
+"TXT_TRUCK_LOCATIONS"        => "Truck Locations",
+"TXT_FREIGHT_DEST"           => "Freight Destinations",
+
+"TXT_ADD_AS_FRIEND"          => "Add as friend",
+"TXT_ADD_AS_ENEMY"           => "Add as enemy",
+"TXT_REAL_LIFE"              => "Real life",
+
+"TXT_CUR_PASSWORD"           => "Current Password",
+"TXT_NEW_PASSWORD"           => "New Password",
+
+"TXT_COMPANY_NAME"           => "Company Name",
+"TXT_CONCERN_NAME"           => "Corporation Name",
+"TXT_CURRENT_NAME"           => "Current Name",
+"TXT_CURRENT_CITY"           => "Current City",
+"TXT_NEW_NAME"               => "New Name",
+"TXT_NEW_CITY"               => "New City",
+
+"TXT_PERSONAL_DETAILS"       => "Personal Details",
+"TXT_PLEASE_SELECT_ONE"      => "Please select one",
+
+"TXT_ACCEPT_FIRE_MEMBERS"    => "Accept and fire members",
+"TXT_ACCEPT_FINISH_PROJECTS" => "Accept and finish projects",
+"TXT_CHANGE_FUEL_PRICE"      => "Change Fuel Price",
+"TXT_UPGRADE_FUEL_STATIONS"  => "Upgrade Fuel Stations",
+"TXT_BUY_FUEL_STATIONS"      => "Buy Fuel Stations",
+"TXT_FILL_FUEL_STATIONS"     => "Fill Fuel Stations",
+
+"TXT_PUBLIC_PRICE"           => "Public Price",
+"TXT_WORKING_HOURS"          => "Working Hours",
+"TXT_SELECT_CITY"            => "Select City",
+"TXT_REFUEL_ALL_TRUCKS"      => "Refuel all trucks",
+"TXT_ON_THE_ROAD"            => "On the road",
+"TXT_PUBLIC_FUEL"            => "Public Fuel",
+"TXT_FUEL_PRICES"            => "Fuel Prices",
+"TXT_MONEY_TRANSFERRED"      => "Money Transferred",
+"TXT_PROJECT_ACTIVITIES"     => "Project Activities",
+"TXT_TOTAL_EARNINGS"         => "Total Earnings",
+
+"TXT_TOO_LATE"               => "Too late",
+"TXT_THIS_FREIGHT"           => "This Freight",
+"TXT_LATE_DELIVERY"          => "Late Delivery",
+"TXT_FAQ"                    => "Frequently Asked Questions",
+
+"TXT_SOCIAL_MEDIA"           => "Social Media",
+
+"TXT_PREMIUM_ACCOUNT"        => "Premium account",
+"TXT_FASTER_ACTION_TIMES"    => "Faster action times",
+"TXT_NO_ADS"                 => "No advertisements",
+"TXT_DELETE_ALL"             => "Delete All",
+"TXT_ADD_TO_BLACKLIST"       => "Add to Blacklist",
+"TXT_NEW_MESSAGE"            => "New message",
+"TXT_DAMAGE_RISK"            => "Damage Risk",
+"TXT_SLEEP_TIME"             => "Sleep time",
+"TXT_PUBLIC_NAME"            => "Public name",
+
+"TXT_PRIVATE_NAME"           => "Private name",
+"TXT_CHANGE_NAME"            => "Change name",
+"TXT_ASSIGN_ALL"             => "Assign All",
+"TXT_NEED_MORE_CREDITS"      => "Need More Credits?",
+"TXT_EXTRA_MONEY"            => "Extra in-game money",
+"TXT_YOUR_EMPLOYEES"         => "Your Employees",
+"TXT_FIRED_FROM_CONCERN"     => "Fired from corporation",
+"TXT_YOURS_FAITHFULLY"       => "Yours Faithfully",
+"TXT_CONCERN_APPLICATION"    => "Corporation Application",
+"TXT_STATUS_UPDATE"          => "Status update",
+
+"TXT_CALLED_IN_SICK"         => "Called in Sick",
+"TXT_GROSS_PROFIT"           => "Gross Profit",
+"TXT_FREIGHT_FINISHED"       => "Freight finished",
+"TXT_FREIGHT_DETAILS"        => "Freight Details",
+"TXT_TRAVEL_TIME"            => "Travel Time",
+"TXT_GRAND_TOTAL"            => "Grand Total",
+"TXT_GROSS_PRICE"            => "Gross Price",
+"TXT_GROSS_TOTAL"            => "Gross Total",
+"TXT_NET_TOTAL"              => "Net Total",
+"TXT_IN_PROGRESS"            => "In progress",
+"TXT_FUEL_THEFT"             => "Fuel Theft",
+
+"TXT_DEFAULT_TRAILER"        => "Default Trailer",
+"TXT_CONTAINER_TRAILER"      => "Container Trailer",
+"TXT_TIPPER_TRAILER"         => "Tipper Trailer",
+"TXT_COOLED_TRAILER"         => "Cooled Trailer",
+"TXT_LIQUID_TRAILER"         => "Liquid Trailer",
+"TXT_LOW_TRAILER"            => "Low Trailer",
+"TXT_CHEMICAL_TRAILER"       => "Chemical Trailer",
+"TXT_RADIOACTIVE_TRAILER"    => "Radioactive Trailer",
+"TXT_HEAVY_TRAILER"          => "Heavy Trailer",
+
+"TXT_DEFAULT_TRANSPORT"      => "Default Transport",
+"TXT_CONTAINER_TRANSPORT"    => "Container Transport",
+"TXT_TIPPER_TRANSPORT"       => "Tipper Transport",
+"TXT_COOLED_TRANSPORT"       => "Cooled Transport",
+"TXT_LIQUID_TRANSPORT"       => "Liquid Transport",
+"TXT_LOW_TRANSPORT"          => "Low Transport",
+"TXT_CHEMICAL_TRANSPORT"     => "Chemical Transport",
+"TXT_RADIOACTIVE_TRANSPORT"  => "Radioactive Transport",
+"TXT_HEAVY_TRANSPORT"        => "Heavy Transport",
+
+"TXT_FREIGHT_CANCELED"       => "Freight canceled",
+"TXT_FREIGHT_ACCEPTED"       => "Freight accepted",
+"TXT_SELECT_A_TRUCK"         => "Select a truck",
+
+"TXT_SALARY_PAYMENT"         => "Salary Payment",
+"TXT_FUEL_COSTS"             => "Fuel Costs",
+"TXT_FREIGHT_EARNINGS"       => "Freight Earnings",
+"TXT_NEW_TRUCKS"             => "New Trucks",
+"TXT_NEW_TRAILERS"           => "New Trailers",
+"TXT_NEW_EMPLOYEES"          => "New Employees",
+"TXT_CONCERN_PAYMENTS"       => "Corporation Payments",
+"TXT_REPAIR_COSTS"           => "Repair Costs",
+"TXT_SOLD_TRUCK_TRAILER"     => "Sold Truck/Trailer",
+"TXT_SHOP_OBJECTS"           => "Shop objects",
+"TXT_CONCERN_DONATION"       => "Corporation Donation",
+
+"TXT_TAXI_COSTS"             => "Taxi Costs",
+"TXT_VIDEO_REWARD"           => "Video Reward",
+"TXT_NEW_BUILDINGS"          => "New Buildings",
+"TXT_DAMAGE_COSTS"           => "Damage Costs",
+"TXT_DAILY_INTEREST"         => "Daily Interest", //As in: money that you get from the bank every day
+"TXT_BUILDING_UPGRADE"       => "Building Upgrade",
+"TXT_BUILDING_DOWNGRADE"     => "Building Downgrade",
+"TXT_FUEL_INCOME"            => "Fuel Income",
+"TXT_NEW_FUEL_STATION"       => "New Fuel Station",
+"TXT_NEW_OFFICE"             => "New Office",
+"TXT_OFFICE_UPGRADE"         => "Office Upgrade",
+"TXT_FUEL_STATION_UPGRADE"   => "Fuel Station Upgrade",
+"TXT_FREIGHT_INCOME"         => "Freight Income",
+"TXT_CANCELED_PROJECTS"      => "Canceled Projects",
+"TXT_COMPLETED_PROJECTS"     => "Completed Projects",
+
+"TXT_IN_MAINTENANCE"         => "In maintenance",
+"TXT_BEING_DELIVERED"        => "Being Delivered",
+"TXT_TRUCK_BOUGHT"           => "Truck Bought",
+"TXT_TRAILER_BOUGHT"         => "Trailer Bought",
+"TXT_FINISHING_FREIGHT"      => "Finishing freight",
+"TXT_DEBT_REPAYMENT"         => "Debt Repayment",
+"TXT_TRANSFERRING_COSTS"     => "Transferring Costs",
+
+"TXT_ACTION_UNTIL"           => "Action until" // : after this
+);
+?>
